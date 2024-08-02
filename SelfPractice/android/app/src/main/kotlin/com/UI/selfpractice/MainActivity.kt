e5fba0d8ea2539406e6932f20d1de00c7867d219
Nurnabi-Hasan <nurnabihasan212@gmail.com>
2024-08-02 19:08:13 +0600

    updated

---
+++ SelfPractice/android/app/src/main/kotlin/com/UI/selfpractice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.UI.selfpractice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ SelfPractice/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+void main (){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LandingPage(),
+      theme: ThemeData(primarySwatch: Colors.grey),
+      darkTheme: ThemeData(primarySwatch: Colors.red),
+
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("My App",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),),
+        backgroundColor: Colors.teal,
+        actions:[
+
+          TextButton(onPressed:(){}, child: Text("Skip",
+            style: TextStyle(color: Colors.white70),))
+        ],
+      ),
+
+      body:Container(
+        padding:EdgeInsets.all(20) ,
+            child: Column(
+
+      ),
+      )
+
+
+
+
+      );
+  }
+
+}
